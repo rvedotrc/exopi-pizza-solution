a9174af4c5ad5d5c427811e88376653fd0583e1b
Rachel Evans <rachel@rachelevans.org>
2026-04-01 08:22:22 +0200

    Vary menu by time

--- PizzaPlace/Services/MenuService.cs
+++ PizzaPlace/Services/MenuService.cs
@@ -4,7 +4,13 @@ public class MenuService : IMenuService
 {
     public Menu GetMenu(DateTimeOffset menuDate)
     {
-        throw new NotImplementedException("No menu has been implemented yet.");
+        bool isLunch = menuDate.Hour >= 11 && menuDate.Hour < 14;
+
+        MenuItem i = new("spam", Models.Types.PizzaRecipeType.OddPizza, isLunch ? 1 : 2);
+
+        return new Menu(
+            isLunch ? "Lunch" : "Not Lunch",
+            [i, i, i, i, i, i, i, i, i, i, i, i,]
+        );
     }
 }
- 
